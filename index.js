65cab037835418068227bb2240138f31ea4bc48c
Troublesome Saramsh <54758588+TroublesomeSaramsh789@users.noreply.github.com>
2020-02-14 21:27:09 +0545

    Final Built
    Every Previous Issues have been fixed.

--- index.js
+++ index.js
@@ -1,178 +1,258 @@
-const winner=[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
-];
-
-
- 
-
-const player1=[];
+function start()
+{
+  const head= document.getElementById('title');
+  const player1=[];
 const player2=[];
 a=0;
+draw=0;
 function play(){
-    
-const box1=document.getElementById('box0');
-    box1.addEventListener("click",()=>{
-        if(a%2!=0){
-        box1.innerHTML='<pr>X</pr><br>';
-        a++;
-        player1.push(1);
-        return 0;
-        }
-        else if(a%2==0){
-        box1.innerHTML='<pr>O<pr>';
-        a++
-        player2.push(1);
-        }
-    });
-    checkA();
-    const box2=document.getElementById('box1');
-    box2.addEventListener("click",()=>{
-        if(a%2!=0){
-        box2.innerHTML='<pr>X</pr><br>';
-        a++;
-        player1.push(2);
-        return 0;
-        }
-        else if(a%2==0){
-        box2.innerHTML='<pr>O<pr>';
-        a++
-        player2.push(2);
-        }
-    });
-    checkA();
-    
-    const box3=document.getElementById('box2');
-    box3.addEventListener("click",()=>{
-        if(a%2!=0){
-        box3.innerHTML='<pr>X</pr><br>';
-        a++;
-        player1.push(3);
-        return 0;
-        }
-        else if(a%2==0){
-        box3.innerHTML='<pr>O<pr>';
-        a++
-        player2.push(3);
-        }
-    });
-    checkA();
-    
-    const box4=document.getElementById('box3');
-    box4.addEventListener("click",()=>{
-        if(a%2!=0){
-        box4.innerHTML='<pr>X</pr><br>';
-        a++;
-        player1.push(4);
-        return 0;
-        }
-        else if(a%2==0){
-        box4.innerHTML='<pr>O<pr>';
-        a++
-        player2.push(4);
-        }
-    });
-    checkA();
-    
-    const box5=document.getElementById('box4');
-    box5.addEventListener("click",()=>{
-        if(a%2!=0){
-        box5.innerHTML='<pr>X</pr><br>';
-        a++;
-        player1.push(5);
-        return 0;
-        }
-        else if(a%2==0){
-        box5.innerHTML='<pr>O<pr>';
-        a++
-        player2.push(5);
-        }
-    });
-    checkA();
-    
-    const box6=document.getElementById('box5');
-    box6.addEventListener("click",()=>{
-        if(a%2!=0){
-        box6.innerHTML='<pr>X</pr><br>';
-        a++;
-        player1.push(6);
-        return 0;
-        }
-        else if(a%2==0){
-        box6.innerHTML='<pr>O<pr>';
-        a++
-        player2.push(6);
-        }
-    });
-    checkA();
-    
-    const box7=document.getElementById('box6');
-    box7.addEventListener("click",()=>{
-        if(a%2!=0){
-        box7.innerHTML='<pr>X</pr><br>';
-        a++;
-        player1.push(7);
-        return 0;
-        }
-        else if(a%2==0){
-        box7.innerHTML='<pr>O<pr>';
-        a++
-        player2.push(7);
-        }
-    });
-    checkA();
+  const box1=document.getElementById('box0');
+      box1.addEventListener("click",function click0(){
+          box1.removeEventListener ("click",click0);
+          if(a%2!=0){
+          box1.innerHTML='<pr>X</pr><br>';
+          a++;
+          player1.push(1);
+          }
+          else if(a%2==0){
+          box1.innerHTML='<pr>O<pr>';
+          a++
+          player2.push(1);
+          }
+          checkA();
+      });
+
+      const box2=document.getElementById('box1');
+      box2.addEventListener("click",function click1(){
+          box2.removeEventListener ("click",click1);
+          if(a%2!=0){
+          box2.innerHTML='<pr>X</pr><br>';
+          a++;
+          player1.push(2);
+          }
+          else if(a%2==0){
+          box2.innerHTML='<pr>O<pr>';
+          a++
+          player2.push(2);
+          }
+          checkA();
+      });
+      
+      const box3=document.getElementById('box2');
+      box3.addEventListener("click",function click2(){
+          box3.removeEventListener ("click",click2);
+          if(a%2!=0){
+          box3.innerHTML='<pr>X</pr><br>';
+          a++;
+          player1.push(3);
+          }
+          else if(a%2==0){
+          box3.innerHTML='<pr>O<pr>';
+          a++
+          player2.push(3);
+          }
+          checkA();
+      });
+
+      const box4=document.getElementById('box3');
+      box4.addEventListener("click",function click3(){
+          box4.removeEventListener ("click",click3);
+          if(a%2!=0){
+          box4.innerHTML='<pr>X</pr><br>';
+          a++;
+          player1.push(4);;
+          }
+          else if(a%2==0){
+          box4.innerHTML='<pr>O<pr>';
+          a++
+          player2.push(4);
+          }
+          checkA();
+      });
+      
+      const box5=document.getElementById('box4');
+      box5.addEventListener("click",function click4(){
+          box5.removeEventListener ("click",click4);
+          if(a%2!=0){
+          box5.innerHTML='<pr>X</pr><br>';
+          a++;
+          player1.push(5);
+          }
+          else if(a%2==0){
+          box5.innerHTML='<pr>O<pr>';
+          a++
+          player2.push(5);
+          }
+          checkA();
+      });
+      
+      const box6=document.getElementById('box5');
+      box6.addEventListener("click",function click5(){
+          box6.removeEventListener ("click",click5);
+          if(a%2!=0){
+          box6.innerHTML='<pr>X</pr><br>';
+          a++;
+          player1.push(6);
+          }
+          else if(a%2==0){
+          box6.innerHTML='<pr>O<pr>';
+          a++
+          player2.push(6);
+          }
+          checkA();
+      });
+      
+      const box7=document.getElementById('box6');
+      box7.addEventListener("click",function click6(){
+          box7.removeEventListener ("click",click6);
+          if(a%2!=0){
+          box7.innerHTML='<pr>X</pr><br>';
+          a++;
+          player1.push(7);
+          }
+          else if(a%2==0){
+          box7.innerHTML='<pr>O<pr>';
+          a++
+          player2.push(7);
+          }
+          checkA();
+      });
     
     const box8=document.getElementById('box7');
-    box8.addEventListener("click",()=>{
+    box8.addEventListener("click",function click7(){
+        box8.removeEventListener ("click",click7);
         if(a%2!=0){
         box8.innerHTML='<pr>X</pr><br>';
         a++;
         player1.push(8);
-        return 0;
         }
         else if(a%2==0){
         box8.innerHTML='<pr>O<pr>';
         a++
         player2.push(8);
         }
+        checkA();
     });
-    checkA();
+    
     
     const box9=document.getElementById('box8');
-    box9.addEventListener("click",()=>{
+    box9.addEventListener("click",function click8(){
+        box9 .removeEventListener ('click',click8);
         if(a%2!=0){
         box9.innerHTML='<pr>X</pr><br>';
         a++;
         player1.push(9);
-        return 0;
         }
         else if(a%2==0){
         box9.innerHTML='<pr>O<pr>';
         a++
         player2.push(9);
         }
+        checkA();
     });
-    checkA();
+    
 
 }
-play();
-function checkA(){if(a>5)
+function winnerA(){
+    const title=document.getElementById('title');
+    title.innerText=('🌟🌟Player 2 Wins🌟🌟');
+    player1=[];
+    player2=[];
+}
+function winnerB(){
+    const title=document.getElementById('title');
+    title.innerText=('🌟🌟Player 1 Wins🌟🌟');
+    player1=[];
+    player2=[];
+}
+function Draw(){
+    const title=document.getElementById('title');
+    title.innerText=("🤯IT'S A DRAW 🤯");
+}
+function checkA()
 {
-    for(var i=0;i<9;i++)
+    if(1)
     {
-        if(winner[i][0]==player1[0] || winner[i][1]==player1[0] || winner[i][2]==player1[0] )
-                if(winner[i][0]==player1[1] || winner[i][1]==player1[2] || winner[i][2]==player1[3] )
-                    if(winner[i][0]==player1[2] || winner[i][1]==player1[3] || winner[i][2]==player1[4] )
-                    {
-                        console.log("player1 wins");
-                    }
+        if(player2.includes(1) && player2.includes(2) && player2.includes(3))
+          {
+            winnerB();
+          }
+          else if(player2.includes(4) && player2.includes(5) && player2.includes(6))
+          {
+            winnerB();
+          }
+          else if(player2.includes(7) && player2.includes(8) && player2.includes(9))
+          {
+            winnerB();
+          }
+          else if(player2.includes(1) && player2.includes(4) && player2.includes(7))
+          {
+            winnerB();
+          }
+          else if(player2.includes(2) && player2.includes(5) && player2.includes(8))
+          {
+            winnerB();
+          }
+          else  if(player2.includes(3) && player2.includes(6) && player2.includes(9))
+          {
+            winnerB();
+          }
+          else if(player2.includes(3) && player2.includes(5) && player2.includes(7))
+          {
+            winnerB();
+          }
+          else if(player2.includes(1) && player2.includes(5) && player2.includes(9))
+          {
+            winnerB();
+          }
+          
+    if(player1.includes(1) && player1.includes(2) && player1.includes(3))
+    {
+      winnerA();
+    }
+    else if(player1.includes(4) && player1.includes(5) && player1.includes(6))
+    {
+      winnerA();
+    }
+    else if(player1.includes(7) && player1.includes(8) && player1.includes(9))
+    {
+      winnerA();
+    }
+    else if(player1.includes(1) && player1.includes(4) && player1.includes(7))
+    {
+      winnerA();
+    }
+    else if(player1.includes(2) && player1.includes(5) && player1.includes(8))
+    {
+      winnerA();
+    }
+    else if(player1.includes(3) && player1.includes(6) && player1.includes(9))
+    {
+      winnerA();
+    }
+    else if(player1.includes(3) && player1.includes(5) && player1.includes(7))
+    {
+      winnerA();
+    }
+    else if(player1.includes(1) && player1.includes(5) && player1.includes(9))
+    {
+      winnerA();
+    }
+    else
+    {
+      draw++;
+      checkdraw();
+    }
+
     }
 }
-};
-         
+function checkdraw()
+{
+  if(draw>8)
+  {
+    head.innerText=("DRAW");
+  }
+} 
+head.innerText=('CLICK TO PLAY');
+play();
+};      
